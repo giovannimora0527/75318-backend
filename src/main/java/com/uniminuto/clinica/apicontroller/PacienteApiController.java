279f917763a138d12f8a8005b2d191fe39565f30
Camila.Salazar <lcsalazar08@gmail.com>
2025-09-06 23:56:51 -0500

    Creacion servicios listar pacientes y buscar por documento

---
+++ src/main/java/com/uniminuto/clinica/apicontroller/PacienteApiController.java
@@ -0,0 +1,27 @@
+package com.uniminuto.clinica.apicontroller;
+
+import com.uniminuto.clinica.entity.Paciente;
+import com.uniminuto.clinica.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteApiController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> listarPacientes() {
+        return ResponseEntity.ok(pacienteService.listarPacientes());
+    }
+
+    @GetMapping("/{numeroDocumento}")
+    public ResponseEntity<Paciente> findByNumeroDocumento(@PathVariable String numeroDocumento) {
+        return ResponseEntity.ok(pacienteService.encontrarPorNumeroDocumento(numeroDocumento));
+    }
+}
---
+++ src/main/java/com/uniminuto/clinica/service/impl/PacienteServiceImpl.java
@@ -0,0 +1,26 @@
+package com.uniminuto.clinica.service.impl;
+
+import com.uniminuto.clinica.entity.Paciente;
+import com.uniminuto.clinica.repository.PacienteRepository;
+import com.uniminuto.clinica.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PacienteServiceImpl implements PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Override
+    public List<Paciente> listarPacientes() {
+        return pacienteRepository.findAll();
+    }
+
+    @Override
+    public Paciente encontrarPorNumeroDocumento(String numeroDocumento) {
+        return pacienteRepository.findByNumeroDocumento(numeroDocumento)
+                .orElseThrow(() -> new RuntimeException("Paciente no encontrado"));
+    }
+}
