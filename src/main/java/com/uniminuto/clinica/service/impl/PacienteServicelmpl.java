b43ba190a4a15afe81d7da0be15b4f5e86fbe8b4
estevez10 <juanpabloestevez98@gmail.com>
2025-09-07 22:06:58 -0500

    Create PacienteServicelmpl.java

---
+++ src/main/java/com/uniminuto/clinica/service/impl/PacienteServicelmpl.java
@@ -0,0 +1,19 @@
+package com.uniminuto.clinica.service.impl;
+
+import com.uniminuto.clinica.entity.Paciente;
+import com.uniminuto.clinica.repository.PacienteRepository;
+import com.uniminuto.clinica.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PacienteServiceImpl implements PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Override
+    public List<Paciente> listarPacientes() {
+        return pacienteRepository.findAll();
+    }
