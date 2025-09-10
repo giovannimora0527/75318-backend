bb4b9d35f9a8d9939b58c00205a362dcae6d5cb6
Mateo-FG <mateoforerog3@gmail.com>
2025-09-09 23:22:32 -0500

    Update PacienteApi.java

--- src/main/java/com/uniminuto/clinica/api/PacienteApi.java
+++ src/main/java/com/uniminuto/clinica/api/PacienteApi.java
@@ -2,25 +2,24 @@ package com.uniminuto.clinica.api;
 
 import com.uniminuto.clinica.entity.Paciente;
 import java.util.List;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+  @author crash
 @CrossOrigin(origins = "*")
 @RequestMapping("/paciente")
 public interface PacienteApi {
 
-    @RequestMapping(value = "/listar",
-            produces = {"application/json"},
-            method = RequestMethod.GET)
-    ResponseEntity<List<Paciente>> listarPacientes();
+    @GetMapping(value = "/obtener-todos", produces = "application/json")
+    ResponseEntity<List<Paciente>> obtenerPacientes();
 
-    @RequestMapping(value = "/buscar",
-            produces = {"application/json"},
-            method = RequestMethod.GET)
-    ResponseEntity<Paciente> buscarPorNumeroDocumento(
-            @RequestParam String numeroDocumento
-    );
+    @GetMapping(value = "/consultar-por-doc", produces = "application/json")
+    ResponseEntity<Paciente> buscarPorDocumento(
+            @RequestParam(name = "numeroDocumento") String numeroDocumento
+    ) throws BadRequestException;
 }
