7d9761921bd224340c22551703fdf86f176b82c7
mauricio-xf <mauricio1234madera@gmail.com>
2025-09-09 21:01:46 -0500

    Create paciente.java

---
+++ src/main/java/com/uniminuto/clinica/entity/paciente.java
@@ -0,0 +1,47 @@
+package com.uniminuto.clinica.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "paciente")
+@Data
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "usuario_id", unique = true)
+    private Integer usuarioId;
+
+    @Column(name = "tipo_documento", nullable = false)
+    private String tipoDocumento;
+
+    @Column(name = "numero_documento", nullable = false, unique = true)
+    private String numeroDocumento;
+
+    @Column(name = "nombres", nullable = false)
+    private String nombres;
+
+    @Column(name = "apellidos", nullable = false)
+    private String apellidos;
+
+    @Column(name = "fecha_nacimiento", nullable = false)
+    private LocalDate fechaNacimiento;
+
+    @Column(name = "genero")
+    private String genero; // 'M', 'F', 'O'
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "direccion")
+    private String direccion;
+}
