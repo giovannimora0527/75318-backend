2ba6ad752c75ab70299eeefdd97ba3d76529f45f
1079977JavierCuervo <j.cuervo1775@gmail.com>
2025-09-12 10:49:44 -0500

    actualizado

--- src/main/java/com/uniminuto/clinica/service/UsuarioService.java
+++ src/main/java/com/uniminuto/clinica/service/UsuarioService.java
@@ -1,8 +1,6 @@
 package com.uniminuto.clinica.service;
 
 import com.uniminuto.clinica.entity.Usuario;
-import com.uniminuto.clinica.model.RespuestaRs;
-import com.uniminuto.clinica.model.UsuarioRq;
 import java.util.List;
 import org.apache.coyote.BadRequestException;
 
@@ -18,6 +16,4 @@ public interface UsuarioService {
   Usuario encontrarPorNombre(String nombreUsuario) throws BadRequestException;
   
   List<Usuario> buscarPorEstado(Integer estado);
-  
-  RespuestaRs guardarUsuario(UsuarioRq usuarioNuevo) throws BadRequestException;
 }
