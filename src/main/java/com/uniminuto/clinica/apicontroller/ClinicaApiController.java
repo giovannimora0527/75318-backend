24dbca82f0f02b874d1d0984f74149e1deed79b9
estevez10 <juanpabloestevez98@gmail.com>
2025-09-20 17:12:44 -0500

    Update ClinicaApiController.java

--- src/main/java/com/uniminuto/clinica/apicontroller/ClinicaApiController.java
+++ src/main/java/com/uniminuto/clinica/apicontroller/ClinicaApiController.java
@@ -1,25 +1,48 @@
 package com.uniminuto.clinica.apicontroller;
 
-import com.uniminuto.clinica.api.ClinicaApi;
 import com.uniminuto.clinica.model.RespuestaRs;
-import com.uniminuto.clinica.service.ClinicaService;
+import com.uniminuto.clinica.model.CitaRq;
+import com.uniminuto.clinica.api.CitaApi;
+import java.util.List;
+import com.uniminuto.clinica.entity.Cita;
+import org.apache.coyote.BadRequestException;
+import com.uniminuto.clinica.service.CitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author lmora
- */
 @RestController
-public class ClinicaApiController implements ClinicaApi {
+public class CitaApiController implements CitaApi {
 
     @Autowired
-    private ClinicaService clinicaService;
+    private CitaService citaService;
 
     @Override
-    public ResponseEntity<RespuestaRs> testService() {
-        return ResponseEntity.ok(clinicaService.testearApp());
+    public ResponseEntity<List<Cita>>
+    listarCitasporPaciente(Long id)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService
+                .buscarPorPaciente(id));
+    }
+
+    @Override
+    public ResponseEntity<List<Cita>>
+    listarCitasporMedico(Long id)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService
+                .buscarPorMedico(id));
+    }
+
+
+    @Override
+    public ResponseEntity<RespuestaRs> guardarCita(CitaRq citaNuevo)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService.guardarCita(citaNuevo));
+    }
+
+    @Override
+    public ResponseEntity<List<Cita>> listarCitasDesc() {
+        return ResponseEntity.ok(this.citaService.listarCitasDesc());
     }
 
 }
