2080e24c4e7d72ffa08e7d91e4c9a496cedd35db
mauricio-xf <mauricio1234madera@gmail.com>
2025-09-19 22:58:17 -0500

    Create pacienteApi.java

---
+++ src/main/java/com/uniminuto/clinica/api/pacienteApi.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.uniminuto.clinica.api;
+
+
+import com.uniminuto.clinica.entity.Paciente;
+import java.util.List;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author crash
+ */
+@CrossOrigin(origins = "*")
+@RequestMapping("/paciente")
+public interface PacienteApi {
+    
+    @RequestMapping(value = "/listar",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.GET)
+    ResponseEntity<List<Paciente>> listarPacientes();
+    //Funcion buscar por documento identidad
+    @RequestMapping(value = "/buscar-NumDoc",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.GET)
+    ResponseEntity<Paciente> encontrarPorDocumentoIdentidad(
+       @RequestParam String numeroDocumento
+    ) throws BadRequestException;
+
+    @RequestMapping(value = "/listarEdadDes",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.GET)
+    ResponseEntity<List<Paciente>> listarPacientesDes();
+}
