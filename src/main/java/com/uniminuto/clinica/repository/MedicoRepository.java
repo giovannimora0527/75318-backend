fa95c851d3807b4320db34f129387abad56f37ac
estevez10 <juanpabloestevez98@gmail.com>
2025-09-20 17:31:09 -0500

    Update MedicoRepository.java

--- src/main/java/com/uniminuto/clinica/repository/MedicoRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/MedicoRepository.java
@@ -1,16 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
 package com.uniminuto.clinica.repository;
 
 import com.uniminuto.clinica.entity.Especializacion;
 import com.uniminuto.clinica.entity.Medico;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  *
- * @author lmora
+ * @author crash
  */
 @Repository
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
+public interface MedicoRepository extends JpaRepository<Medico,Long>{
+    Optional<Medico> findById(Long id);
+
     List<Medico> findByEspecializacion(Especializacion e);
 }
