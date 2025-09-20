391b66d632cd606c971a7239e888220d8b670eee
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 09:18:42 -0500

    Update RecetaRq.java

--- src/main/java/com/uniminuto/clinica/model/RecetaRq.java
+++ src/main/java/com/uniminuto/clinica/model/RecetaRq.java
@@ -1,1 +1,16 @@
+package com.uniminuto.clinica.model;
+import com.uniminuto.clinica.entity.Cita;
+import com.uniminuto.clinica.entity.Medicamento;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Data
+public class RecetaRq {
+
+    private Long id;
+    private String dosis;
+    private String indicaciones;
+    private Cita cita;
+    private Medicamento medicamento;
+}
