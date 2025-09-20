b0929c4e06f923d09cc977bdcdbfc7f4d00a8253
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 17:29:06 -0500

    Update Paciente.java

--- src/main/java/com/uniminuto/clinica/entity/Paciente.java
+++ src/main/java/com/uniminuto/clinica/entity/Paciente.java
@@ -1,48 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
+ */
 package com.uniminuto.clinica.entity;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import javax.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
 
-@Getter
-@Setter
+/**
+ *
+ * @author crash
+ */
+@Data
 @Entity
 @Table(name = "paciente")
-public class Paciente implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
+public class Paciente implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
-
-    @Column(name = "id_usuario")
-    private Integer idUsuario;
-
-    @Column(name = "tipo_documento", nullable = false, length = 20)
+    private Long id;
+    
+    @Column(name = "usuario_id")
+    private Integer usuarioId;
+    
+    @Column(name = "tipo_documento")
     private String tipoDocumento;
-
-    @Column(name = "numero_documento", unique = true, nullable = false, length = 50)
+    
+    
+    @Column(name = "numero_documento")
     private String numeroDocumento;
-
-    @Column(name = "nombres", nullable = false, length = 100)
+    
+    @Column(name = "nombres")
     private String nombres;
-
-    @Column(name = "apellidos", nullable = false, length = 100)
+    
+    
+    @Column(name = "apellidos")
     private String apellidos;
-
+    
     @Column(name = "fecha_nacimiento")
     private LocalDate fechaNacimiento;
+    
+    @Column(name = "genero")
+    private char genero;
 
-    @Column(name = "genero", length = 10)
-    private String genero;
-
-    @Column(name = "telefono", length = 20)
+    @Column(name = "telefono")
     private String telefono;
-
-    @Column(name = "direccion", length = 200)
+    
+    
+    @Column(name = "direccion")
     private String direccion;
+    
 }
