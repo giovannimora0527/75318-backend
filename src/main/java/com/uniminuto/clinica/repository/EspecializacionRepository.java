270b836dfa874e05033c74999065a65027f13253
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 17:36:28 -0500

    Update EspecializacionRepository.java

--- src/main/java/com/uniminuto/clinica/repository/EspecializacionRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/EspecializacionRepository.java
@@ -1,3 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
 package com.uniminuto.clinica.repository;
 
 import com.uniminuto.clinica.entity.Especializacion;
@@ -7,11 +12,11 @@ import org.springframework.stereotype.Repository;
 
 /**
  *
- * @author lmora
+ * @author crash
  */
 @Repository
 public interface EspecializacionRepository 
-        extends JpaRepository<Especializacion, Long> {
+    extends JpaRepository<Especializacion, Long>{
     
     Optional<Especializacion> findByCodigoEspecializacion(String codigo);
     
