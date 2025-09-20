5813912812102da3d31dd39246a715e6aaffef45
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 09:22:08 -0500

    Update CitaRepository.java

--- src/main/java/com/uniminuto/clinica/repository/CitaRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/CitaRepository.java
@@ -1,1 +1,15 @@
+package com.uniminuto.clinica.repository;
 
+import com.uniminuto.clinica.entity.Cita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CitaRepository extends JpaRepository<Cita, Long> {
+    Optional<Cita> findByMotivo(String motivo);
+
+    List<Cita> findAllByOrderByFechaHoraDesc();
+}
