91d71062bd1bc31e26476c5d90c5649aa3fb7499
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 09:17:42 -0500

    Update CitaRq.java

--- src/main/java/com/uniminuto/clinica/model/CitaRq.java
+++ src/main/java/com/uniminuto/clinica/model/CitaRq.java
@@ -1,1 +1,18 @@
+package com.uniminuto.clinica.model;
+import com.uniminuto.clinica.entity.Medico;
+import com.uniminuto.clinica.entity.Paciente;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+public class CitaRq {
+
+    private Long id;
+    private LocalDate fechaHora;
+    private String motivo;
+    private String estado;
+    private Paciente paciente;
+    private Medico medico;
+}
