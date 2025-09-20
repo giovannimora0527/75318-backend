f136b82d76ab7106f1d87b37e83feb92283f6bb6
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 08:50:08 -0500

    Update ClinicaApi.java

--- src/main/java/com/uniminuto/clinica/api/ClinicaApi.java
+++ src/main/java/com/uniminuto/clinica/api/ClinicaApi.java
@@ -1,29 +1,23 @@
 package com.uniminuto.clinica.api;
 
-import com.uniminuto.clinica.entity.Cita;
-import com.uniminuto.clinica.model.CitaRq;
 import com.uniminuto.clinica.model.RespuestaRs;
-import org.apache.coyote.BadRequestException;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
-
+/**
+ *
+ * @author lmora
+ */
 @CrossOrigin(origins = "*")
-@RequestMapping("/cita")
-public interface CitaApi {
+@RequestMapping("/clinica")
+public interface ClinicaApi {
 
-    @RequestMapping(value = "/guardar",
+    @RequestMapping(value = "/test",
             produces = {"application/json"},
             consumes = {"application/json"},
-            method = RequestMethod.POST)
-    ResponseEntity<RespuestaRs> guardarCita(
-            @RequestBody CitaRq citaRq)
-            throws BadRequestException;
-    @RequestMapping(value = "/listar-recientes",
-            produces = {"application/json"},
-            method = RequestMethod.GET
-    )
-    ResponseEntity<List<Cita>> listarCitasRecientes();
+            method = RequestMethod.GET)
+    ResponseEntity<RespuestaRs> testService();
+
 }
