49aad2c522cdfe5995d985c5948543e4baf81e7a
estevez10 <juanpabloestevez98@gmail.com>
2025-09-20 17:16:05 -0500

    Update CitaApi.java

--- src/main/java/com/uniminuto/clinica/api/CitaApi.java
+++ src/main/java/com/uniminuto/clinica/api/CitaApi.java
@@ -1,10 +1,9 @@
 package com.uniminuto.clinica.api;
 
-import com.uniminuto.clinica.entity.Cita;
-import com.uniminuto.clinica.model.CitaRq;
 import com.uniminuto.clinica.model.RespuestaRs;
+import com.uniminuto.clinica.model.CitaRq;
+import com.uniminuto.clinica.entity.Cita;
 import org.apache.coyote.BadRequestException;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +12,35 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RequestMapping("/cita")
 public interface CitaApi {
+    @RequestMapping(value = "/listar-por-paciente",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.GET)
+    ResponseEntity<List<Cita>> listarCitasporPaciente(
+            @RequestParam Long id
+    ) throws BadRequestException;
+
+    @RequestMapping(value = "/listar-por-medico",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.GET)
+    ResponseEntity<List<Cita>> listarCitasporMedico(
+            @RequestParam Long id
+    ) throws BadRequestException;
 
     @RequestMapping(value = "/guardar",
             produces = {"application/json"},
             consumes = {"application/json"},
             method = RequestMethod.POST)
     ResponseEntity<RespuestaRs> guardarCita(
+            @RequestBody CitaRq citaNuevo
+    ) throws BadRequestException;
+
+    @RequestMapping(value = "/listar-desc",
+            produces = {"application/json"},
+            method = RequestMethod.GET)
+    ResponseEntity<List<Cita>> listarCitasDesc();
+}
             @RequestBody CitaRq citaRq)
             throws BadRequestException;
     @RequestMapping(value = "/listar-recientes",
