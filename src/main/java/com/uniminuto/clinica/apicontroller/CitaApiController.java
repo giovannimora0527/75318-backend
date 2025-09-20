41896caa743ae8f96bea882c0e0b703f4322cf81
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 17:20:39 -0500

    Update CitaApiController.java

--- src/main/java/com/uniminuto/clinica/apicontroller/CitaApiController.java
+++ src/main/java/com/uniminuto/clinica/apicontroller/CitaApiController.java
@@ -1,16 +1,15 @@
 package com.uniminuto.clinica.apicontroller;
 
-import com.uniminuto.clinica.api.CitaApi;
-import com.uniminuto.clinica.entity.Cita;
-import com.uniminuto.clinica.model.CitaRq;
 import com.uniminuto.clinica.model.RespuestaRs;
-import com.uniminuto.clinica.service.CitaService;
+import com.uniminuto.clinica.model.CitaRq;
+import com.uniminuto.clinica.api.CitaApi;
+import java.util.List;
+import com.uniminuto.clinica.entity.Cita;
 import org.apache.coyote.BadRequestException;
+import com.uniminuto.clinica.service.CitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class CitaApiController implements CitaApi {
@@ -19,13 +18,31 @@ public class CitaApiController implements CitaApi {
     private CitaService citaService;
 
     @Override
-    public ResponseEntity<RespuestaRs> guardarCita(CitaRq citaRq)
+    public ResponseEntity<List<Cita>>
+    listarCitasporPaciente(Long id)
             throws BadRequestException {
-        return ResponseEntity.ok(this.citaService.guardarCita(citaRq));
+        return ResponseEntity.ok(this.citaService
+                .buscarPorPaciente(id));
+    }
 
-    }
     @Override
-    public ResponseEntity<List<Cita>> listarCitasRecientes() {
-        return ResponseEntity.ok(citaService.listarCitasRecientes());
+    public ResponseEntity<List<Cita>>
+    listarCitasporMedico(Long id)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService
+                .buscarPorMedico(id));
     }
+
+
+    @Override
+    public ResponseEntity<RespuestaRs> guardarCita(CitaRq citaNuevo)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService.guardarCita(citaNuevo));
+    }
+
+    @Override
+    public ResponseEntity<List<Cita>> listarCitasDesc() {
+        return ResponseEntity.ok(this.citaService.listarCitasDesc());
+    }
+
 }
