0243ee43974e32e6daef1c32dea7ba7e72088bc1
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 17:22:29 -0500

    Update MedicamentoApiController.java

--- src/main/java/com/uniminuto/clinica/apicontroller/MedicamentoApiController.java
+++ src/main/java/com/uniminuto/clinica/apicontroller/MedicamentoApiController.java
@@ -1,35 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
 package com.uniminuto.clinica.apicontroller;
 
-import com.uniminuto.clinica.api.MedicamentoApi;
-import com.uniminuto.clinica.entity.Medicamento;
-import com.uniminuto.clinica.model.MedicamentoRq;
-import com.uniminuto.clinica.model.RespuestaRs;
-import com.uniminuto.clinica.service.MedicamentoService;
+import com.uniminuto.clinica.api.MedicoApi;
+import com.uniminuto.clinica.entity.Medico;
+import com.uniminuto.clinica.service.MedicoService;
+import java.util.List;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+/**
+ *
+<<<<<<< HEAD
+ * @author crash
+ */
 @RestController
-public class MedicamentoApiController implements MedicamentoApi {
-
+public class MedicoApiController implements MedicoApi{   
     @Autowired
-    private MedicamentoService medicamentoService;
+    private MedicoService medicoService;
 
     @Override
-    public ResponseEntity<List<Medicamento>> listarMedicamentos() {
-        return ResponseEntity.ok(this.medicamentoService.listarMedicamentos());
+    public ResponseEntity<List<Medico>> listarMedicos() {
+        return ResponseEntity.ok(this.medicoService.listarMedicos());
     }
 
     @Override
-    public ResponseEntity<RespuestaRs> guardarMedicamento(MedicamentoRq medicamentoRq) throws BadRequestException {
-        return ResponseEntity.ok(medicamentoService.guardarMedicamento(medicamentoRq));
+    public ResponseEntity<List<Medico>>
+            listarMedicosporEspecialidad(String codigo)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.medicoService
+                .buscarPorEspecialidad(codigo));
     }
 
     @Override
-    public ResponseEntity<RespuestaRs> actualizarMedicamento(MedicamentoRq medicamentoRq) throws BadRequestException {
-        return ResponseEntity.ok(medicamentoService.actualizarMedicamento(medicamentoRq));
+    public ResponseEntity<Medico> buscarMedicoId(Long id)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.medicoService.buscarMedicoId(id));
     }
 }
