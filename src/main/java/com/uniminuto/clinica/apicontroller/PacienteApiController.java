58c147a254e229b445072875f66b0e51eac78e9a
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 09:08:13 -0500

    Update PacienteApiController.java

--- src/main/java/com/uniminuto/clinica/apicontroller/PacienteApiController.java
+++ src/main/java/com/uniminuto/clinica/apicontroller/PacienteApiController.java
@@ -1,36 +1,33 @@
 package com.uniminuto.clinica.apicontroller;
 
+import java.util.List;
+
 import com.uniminuto.clinica.api.PacienteApi;
 import com.uniminuto.clinica.entity.Paciente;
 import com.uniminuto.clinica.service.PacienteService;
-import java.util.List;
 import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author crash
- */
 @RestController
 public class PacienteApiController implements PacienteApi {
 
-    private final PacienteService pacienteService;
+    @Autowired
+    private PacienteService pacienteService;
 
-    public PacienteApiController(PacienteService pacienteService) {
-        this.pacienteService = pacienteService;
+    @Override
+    public ResponseEntity<List<Paciente>> listarPacientes() {
+        return ResponseEntity.ok(this.pacienteService.listarPacientes());
     }
 
     @Override
-    public ResponseEntity<List<Paciente>> obtenerPacientes() {
-        List<Paciente> pacientes = pacienteService.listarTodo();
-        return ResponseEntity.ok(pacientes);
-    }
-
-    @Override
-    public ResponseEntity<Paciente> buscarPorDocumento(String numeroDocumento) 
+    public ResponseEntity<Paciente> buscarPorDocumento(String numeroDocumento)
             throws BadRequestException {
-        Paciente paciente = pacienteService.encontrarPorDocumentoIdentidad(numeroDocumento);
-        return ResponseEntity.ok(paciente);
+        return ResponseEntity.ok(this.pacienteService.buscarPorDocumento(numeroDocumento));
+    }
+    @Override
+    public ResponseEntity<List<Paciente>> listarPacientesPorEdadAsc() {
+        return ResponseEntity.ok(pacienteService.listarPacientesPorEdadAsc());
     }
 }
