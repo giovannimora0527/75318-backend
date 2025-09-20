cc636ada01c00da2e0fa08b2329f6ed5beec83f6
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 09:22:54 -0500

    Update RecetaRepository.java

--- src/main/java/com/uniminuto/clinica/repository/RecetaRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/RecetaRepository.java
@@ -1,1 +1,15 @@
+package com.uniminuto.clinica.repository;
 
+import com.uniminuto.clinica.entity.Cita;
+import com.uniminuto.clinica.entity.Receta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RecetaRepository extends JpaRepository<Receta, Long> {
+    Optional<Receta> findById(Integer Id);
+
+}
