85e63b60bfc3ee7a0d4c9aac7fa2ddfa04a71670
estevez10 <juanpabloestevez98@gmail.com>
2025-09-20 17:21:29 -0500

    Update RecetaApiController.java

--- src/main/java/com/uniminuto/clinica/apicontroller/RecetaApiController.java
+++ src/main/java/com/uniminuto/clinica/apicontroller/RecetaApiController.java
@@ -1,5 +1,7 @@
 package com.uniminuto.clinica.apicontroller;
+
 import com.uniminuto.clinica.api.RecetaApi;
+import com.uniminuto.clinica.entity.Receta;
 import com.uniminuto.clinica.model.RecetaRq;
 import com.uniminuto.clinica.model.RespuestaRs;
 import com.uniminuto.clinica.service.RecetaService;
@@ -11,15 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class RecetaApiController implements RecetaApi {
-
+public class RecetaApiController implements RecetaApi{
     @Autowired
     private RecetaService recetaService;
 
     @Override
-    public ResponseEntity<RespuestaRs> crearReceta(RecetaRq recetaRq)
-            throws BadRequestException {
-        return ResponseEntity.ok(this.recetaService.crearReceta(recetaRq));
+    public ResponseEntity<List<Receta>> listarTodasRecetas() {
+        return ResponseEntity.ok(recetaService.listarTodasRecetas());
+    }
 
+    @Override
+    public ResponseEntity<List<Receta>> listarRecetasDesc() {
+        return ResponseEntity.ok(recetaService.listarRecetasDesc());
+    }
+
+    @Override
+    public ResponseEntity<List<Receta>> listarRecetasPorCita(Long citaId)
+            throws BadRequestException {
+        return ResponseEntity.ok(recetaService.buscarPorCita(citaId));
+    }
+
+    @Override
+    public ResponseEntity<List<Receta>> listarRecetasPorMedicamento(Integer medicamentoId) {
+        return ResponseEntity.ok(recetaService.buscarPorMedicamento(medicamentoId));
+    }
+
+    @Override
+    public ResponseEntity<Receta> buscarRecetaPorId(Long id) throws BadRequestException {
+        return ResponseEntity.ok(recetaService.buscarRecetaPorId(id));
+    }
+
+    @Override
+    public ResponseEntity<RespuestaRs> guardarReceta(RecetaRq recetaRq)
+            throws BadRequestException {
+        return ResponseEntity.ok(recetaService.guardarReceta(recetaRq));
     }
 }
