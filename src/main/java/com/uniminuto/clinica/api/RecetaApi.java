9377280450273a01d41986708b6b3d8386e989ba
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 08:56:14 -0500

    Update RecetaApi.java

--- src/main/java/com/uniminuto/clinica/api/RecetaApi.java
+++ src/main/java/com/uniminuto/clinica/api/RecetaApi.java
@@ -1,1 +1,23 @@
+package com.uniminuto.clinica.api;
 
+import com.uniminuto.clinica.model.RecetaRq;
+import com.uniminuto.clinica.model.RespuestaRs;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("/receta")
+public interface RecetaApi {
+
+    @RequestMapping(value = "/crear",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.POST)
+    ResponseEntity<RespuestaRs> crearReceta(
+            @RequestBody RecetaRq recetaRq)
+            throws BadRequestException;
+}
