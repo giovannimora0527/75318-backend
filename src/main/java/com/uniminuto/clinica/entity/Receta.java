7cc0d651e1c9632c1fcef570a169a061f4f70766
Mateo-FG <mateoforerog3@gmail.com>
2025-09-20 09:13:33 -0500

    Update Receta.java

--- src/main/java/com/uniminuto/clinica/entity/Receta.java
+++ src/main/java/com/uniminuto/clinica/entity/Receta.java
@@ -1,1 +1,33 @@
+package com.uniminuto.clinica.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+@Data
+@Entity
+@Table(name = "receta")
+public class Receta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "dosis")
+    private String dosis;
+
+    @Column(name = "indicaciones")
+    private String indicaciones;
+
+    @ManyToOne
+    @JoinColumn(name = "cita_id")
+    private Cita cita;
+
+    @ManyToOne
+    @JoinColumn(name = "medicamento_id")
+    private Medicamento medicamento;
+}
