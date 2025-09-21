7c02312f409f1855a54ec43b31407d30395a7a4b
mauricio-xf <mauricio1234madera@gmail.com>
2025-09-20 22:30:29 -0500

    Create CitaApiController.java

---
+++ src/main/java/com/uniminuto/clinica/apicontroller/CitaApiController.java
@@ -0,0 +1,48 @@
+package com.uniminuto.clinica.apicontroller;
+
+import com.uniminuto.clinica.model.RespuestaRs;
+import com.uniminuto.clinica.model.CitaRq;
+import com.uniminuto.clinica.api.CitaApi;
+import java.util.List;
+import com.uniminuto.clinica.entity.Cita;
+import org.apache.coyote.BadRequestException;
+import com.uniminuto.clinica.service.CitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CitaApiController implements CitaApi {
+
+    @Autowired
+    private CitaService citaService;
+
+    @Override
+    public ResponseEntity<List<Cita>>
+    listarCitasporPaciente(Long id)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService
+                .buscarPorPaciente(id));
+    }
+
+    @Override
+    public ResponseEntity<List<Cita>>
+    listarCitasporMedico(Long id)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService
+                .buscarPorMedico(id));
+    }
+
+
+    @Override
+    public ResponseEntity<RespuestaRs> guardarCita(CitaRq citaNuevo)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService.guardarCita(citaNuevo));
+    }
+
+    @Override
+    public ResponseEntity<List<Cita>> listarCitasDesc() {
+        return ResponseEntity.ok(this.citaService.listarCitasDesc());
+    }
+
+}
