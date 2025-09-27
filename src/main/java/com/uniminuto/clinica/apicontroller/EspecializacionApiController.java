2f597584117e99dc5ad53a33a738eaf216384ffb
mauricio-xf <mauricio1234madera@gmail.com>
2025-09-27 14:30:40 -0500

    Update EspecializacionApiController.java

--- src/main/java/com/uniminuto/clinica/apicontroller/EspecializacionApiController.java
+++ src/main/java/com/uniminuto/clinica/apicontroller/EspecializacionApiController.java
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+=======
+>>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 package com.uniminuto.clinica.apicontroller;
 
 import com.uniminuto.clinica.api.EspecializacionApi;
