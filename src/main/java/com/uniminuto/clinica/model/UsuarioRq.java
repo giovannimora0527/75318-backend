845d316b40a2572593cd92f18d0f37e779b4da76
mauricio-xf <mauricio1234madera@gmail.com>
2025-09-27 16:05:16 -0500

    Update UsuarioRq.java

--- src/main/java/com/uniminuto/clinica/model/UsuarioRq.java
+++ src/main/java/com/uniminuto/clinica/model/UsuarioRq.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.uniminuto.clinica.model;
 
 import lombok.Data;
@@ -6,9 +7,30 @@ import lombok.Data;
  *
  * @author lmora
  */
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uniminuto.clinica.model;
+
+/**
+ *
+ * @author darle
+ */
+
+
+import lombok.Data;
+
+
+>>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 @Data
 public class UsuarioRq {
     private String username;
     private String pass;
     private String rol;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
