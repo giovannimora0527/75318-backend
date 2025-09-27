a80630b45fbdc4b79275fa99f7dee4f5b23de92c
mauricio-xf <mauricio1234madera@gmail.com>
2025-09-27 16:12:08 -0500

    Update EspecializacionService.java

--- src/main/java/com/uniminuto/clinica/service/EspecializacionService.java
+++ src/main/java/com/uniminuto/clinica/service/EspecializacionService.java
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+=======
+>>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 package com.uniminuto.clinica.service;
 
 import com.uniminuto.clinica.entity.Especializacion;
