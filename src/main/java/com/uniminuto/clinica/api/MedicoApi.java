9df6b5c1a86a3137a7cd9c25630f055416edb43d
Fabio :3 <otalvaromaldonadof@gmail.com>
2025-10-24 23:51:03 -0500

    Update MedicoApi.java

--- src/main/java/com/uniminuto/clinica/api/MedicoApi.java
+++ src/main/java/com/uniminuto/clinica/api/MedicoApi.java
@@ -2,12 +2,12 @@ package com.uniminuto.clinica.api;
 
 import com.uniminuto.clinica.entity.Medico;
 import java.util.List;
+
+import com.uniminuto.clinica.model.MedicoRq;
+import com.uniminuto.clinica.model.RespuestaRs;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -31,4 +31,37 @@ public interface MedicoApi {
     ResponseEntity<List<Medico>> listarMedicosporEspecialidad(
       @RequestParam String codigo
     ) throws BadRequestException;
+
+    @RequestMapping(value = "/buscar-medico-documento",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.GET)
+    ResponseEntity<Medico> buscarMedicoPorDocumento(
+            @RequestParam String documento)
+            throws BadRequestException;
+
+
+    /*
+     * TODO Agregar medodo para guardar medico.*/
+    @RequestMapping(value = "/guardar",
+        produces = {"application/json"},
+        consumes = {"application/json"},
+        method = RequestMethod.POST)
+    ResponseEntity<RespuestaRs> guardarMedico(
+       @RequestBody MedicoRq medicoNuevo
+    ) throws BadRequestException;
+
+
+
+
+    /*
+     * TODO agregar metodo para actualizar medico.*/
+    @RequestMapping(value = "/actualizar",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.POST)
+    ResponseEntity<RespuestaRs> actualizarMedico(
+            @RequestBody MedicoRq medico
+    ) throws BadRequestException;
+
 }
