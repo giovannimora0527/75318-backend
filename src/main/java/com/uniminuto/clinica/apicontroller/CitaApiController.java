afb6cbfba46ae5634936ac094c5a033192de03fe
estevez10 <juanpabloestevez98@gmail.com>
2025-10-25 22:11:51 -0500

    Update CitaApiController.java

--- src/main/java/com/uniminuto/clinica/apicontroller/CitaApiController.java
+++ src/main/java/com/uniminuto/clinica/apicontroller/CitaApiController.java
@@ -1,48 +1,41 @@
 package com.uniminuto.clinica.apicontroller;
 
-import com.uniminuto.clinica.model.RespuestaRs;
-import com.uniminuto.clinica.model.CitaRq;
 import com.uniminuto.clinica.api.CitaApi;
-import java.util.List;
 import com.uniminuto.clinica.entity.Cita;
-import org.apache.coyote.BadRequestException;
+import com.uniminuto.clinica.model.CitaRq;
+import com.uniminuto.clinica.model.RespuestaRs;
 import com.uniminuto.clinica.service.CitaService;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class CitaApiController implements CitaApi {
 
+    /**
+     * Servicio de citas.
+     */
     @Autowired
     private CitaService citaService;
 
     @Override
-    public ResponseEntity<List<Cita>>
-    listarCitasporPaciente(Long id)
-            throws BadRequestException {
-        return ResponseEntity.ok(this.citaService
-                .buscarPorPaciente(id));
+    public ResponseEntity<List<Cita>> listarCitas() {
+        return ResponseEntity.ok(this.citaService.listarCitas());
     }
 
     @Override
-    public ResponseEntity<List<Cita>>
-    listarCitasporMedico(Long id)
-            throws BadRequestException {
-        return ResponseEntity.ok(this.citaService
-                .buscarPorMedico(id));
-    }
-
-
-    @Override
-    public ResponseEntity<RespuestaRs> guardarCita(CitaRq citaNuevo)
-            throws BadRequestException {
-        return ResponseEntity.ok(this.citaService.guardarCita(citaNuevo));
+    public ResponseEntity<RespuestaRs> guardarCita(@RequestBody @Valid CitaRq citaRq) throws BadRequestException {
+        return ResponseEntity.ok(this.citaService.guardarCita(citaRq));
     }
 
     @Override
-    public ResponseEntity<List<Cita>> listarCitasDesc() {
-        return ResponseEntity.ok(this.citaService.listarCitasDesc());
+    public ResponseEntity<RespuestaRs> actualizarCita(@RequestBody @Valid CitaRq citaRq)
+            throws BadRequestException {
+        return ResponseEntity.ok(this.citaService.actualizarCita(citaRq));
     }
-
 }
