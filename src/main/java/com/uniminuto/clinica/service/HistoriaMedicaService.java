592d5a9fb547a7527c214d749af2e9aec17854c4
Mateo-FG <mateoforerog3@gmail.com>
2025-10-25 22:46:41 -0500

    Create HistoriaMedicaService.java

---
+++ src/main/java/com/uniminuto/clinica/service/HistoriaMedicaService.java
@@ -0,0 +1,11 @@
+package com.uniminuto.clinica.service;
+
+import com.uniminuto.clinica.entity.HistoriaMedica;
+
+import java.util.List;
+
+public interface HistoriaMedicaService {
+
+    List<HistoriaMedica> listarHistoriaMedica();
+
+}
