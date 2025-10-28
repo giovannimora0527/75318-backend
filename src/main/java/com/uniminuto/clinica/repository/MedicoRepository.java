27f5cbf772cda8ddc4e1f2d469ce10d7f26b1126
mauricio-xf <mauricio1234madera@gmail.com>
2025-10-28 14:17:46 -0500

    Update MedicoRepository.java

--- src/main/java/com/uniminuto/clinica/repository/MedicoRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/MedicoRepository.java
@@ -1,38 +1,23 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
-=======
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 package com.uniminuto.clinica.repository;
 
 import com.uniminuto.clinica.entity.Especializacion;
 import com.uniminuto.clinica.entity.Medico;
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Optional;
 
-=======
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
+import com.uniminuto.clinica.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  *
-<<<<<<< HEAD
- * @author crash
- */
-@Repository
-public interface MedicoRepository extends JpaRepository<Medico,Long>{
-    Optional<Medico> findById(Long id);
-
-=======
  * @author lmora
  */
 @Repository
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
+public interface MedicoRepository extends JpaRepository<Medico, Integer> {
+
+    Optional<Medico> findByDocumento(String documento);
+
     List<Medico> findByEspecializacion(Especializacion e);
+
 }
