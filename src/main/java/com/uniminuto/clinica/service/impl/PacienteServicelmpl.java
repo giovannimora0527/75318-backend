9df5572aa42d6e7005e09c78642593e4eb7f900c
mauricio-xf <mauricio1234madera@gmail.com>
2025-10-28 15:43:20 -0500

    Update PacienteServicelmpl.java

--- src/main/java/com/uniminuto/clinica/service/impl/PacienteServicelmpl.java
+++ src/main/java/com/uniminuto/clinica/service/impl/PacienteServicelmpl.java
@@ -1,11 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.uniminuto.clinica.service.impl;
 
-
 import com.uniminuto.clinica.entity.Paciente;
+import com.uniminuto.clinica.model.PacienteRq;
+import com.uniminuto.clinica.model.RespuestaRs;
 import com.uniminuto.clinica.repository.PacienteRepository;
 import com.uniminuto.clinica.service.PacienteService;
 import java.util.List;
@@ -16,85 +13,159 @@ import org.springframework.stereotype.Service;
 
 /**
  *
- *
- *
- * @author crash
+ * @author lmora
  */
 @Service
 public class PacienteServiceImpl implements PacienteService {
-    /**
-     * Repositorio JPA para operaciones de persistencia de la entidad Paciente.
-     */
+
     @Autowired
-    private PacienteRepository PacienteRepository;
-    /**
-     * Implementa el ordenamiento utilizando Java 8 Streams y el método compareTo
-     * de LocalDate para garantizar un orden cronológico preciso.
-     * @return List<Paciente> Lista ordenada donde el primer elemento es el paciente
-     * con la fecha de nacimiento más antigua
-     */
+    private PacienteRepository pacienteRepository;
+
     @Override
-    public List<Paciente> listarPacientesDes() {
-        return PacienteRepository.findAll()
-                /**
-                 * Stream que permite operaciones funcionales sobre la colección de pacientes
-                 */
-                .stream()
-                /**
-                 * Operación de ordenamiento que compara fechas de nacimiento.
-                 * Utiliza expresión lambda (m1, m2) donde:
-                 * - m1: primer paciente en la comparación
-                 * - m2: segundo paciente en la comparación
-                 * - compareTo: devuelve -1, 0, o 1 según la fecha sea anterior, igual o posterior
-                 */
-                .sorted((m1, m2) -> m1.getFechaNacimiento().compareTo(m2.getFechaNacimiento()))
-                /**
-                 * Convierte el Stream ordenado de nuevo a una lista
-                 */
-                .toList();
+    public List<Paciente> encontrarTodosLosPacientes() {
+        return this.pacienteRepository.findAll();
     }
-    //* Utiliza el método findAll() del repositorio JPA para obtener todos los registros
+
     @Override
-    public List<Paciente> listarTodo() {
-        return this.PacienteRepository.findAll();
-    }
-    //Funcion buscar por documento identidad
-    /*
-     * Utiliza Optional para manejar de forma segura la posible ausencia del paciente.
-     * Sigue el patrón de programación defensiva para evitar NullPointerException.
-    * */
-    @Override
-    public Paciente encontrarPorDocumentoIdentidad(String numeroDocumento)
-            throws BadRequestException {
-        /**
-         * Optional<Paciente> contenedor que puede o no contener un paciente
-         * Evita el riesgo de retornar null directamente
-         */
-        Optional<Paciente> optUser = this.PacienteRepository
-                .findByNumeroDocumento(numeroDocumento);
-        //Validación de existencia del paciente antes de retornarlo
-        if (!optUser.isPresent()) {
-            throw new BadRequestException("No existe el usuario con el documento de identidad: " + numeroDocumento);
+    public Paciente buscarPacientePorDocumento(String documento) throws BadRequestException {
+        Optional<Paciente> optPaciente = this.pacienteRepository.findByNumeroDocumento(documento);
+        if (!optPaciente.isPresent()) {
+            throw new BadRequestException("No se encuentra el paciente");
         }
-        //Extracción segura del objeto Paciente del Optional
-        return optUser.get();
+        return optPaciente.get();
     }
-/*
- * Implementación similar al método de búsqueda por documento pero utilizando
- * el identificador primario de la base de datos.
-*/
+
     @Override
-    //Búsqueda por clave primaria utilizando el método heredado de JpaRepository
-    public Paciente buscarPacienteId(Long id) throws BadRequestException {
-        /**
-         * Optional<Paciente> contenedor que puede o no contener un paciente
-         * Evita el riesgo de retornar null directamente
-         */
-        Optional<Paciente> optUser = this.PacienteRepository.findById(id);
-        if (!optUser.isPresent()) {
-            throw new BadRequestException("No existe el usuario con el id: " + id);
+    public List<Paciente> listarOrdenadoPorFechaNacimiento(boolean ascendente) {
+        if (ascendente) {
+            return this.pacienteRepository.findAllByOrderByFechaNacimientoAsc();
+        } else {
+            return this.pacienteRepository.findAllByOrderByFechaNacimientoDesc();
+        }
+    }
+
+
+    @Override
+    public RespuestaRs guardarPaciente(PacienteRq pacienteNuevo)
+            throws BadRequestException {
+        // Paso 1. Validar que los campos llegue bien
+        this.validarCampos(pacienteNuevo);
+        // Paso 2. Consulto si existe el paciente por numero documento
+
+        Optional<Paciente> optPaciente = this.pacienteRepository.
+            findByNumeroDocumento(pacienteNuevo.getNumeroDocumento());
+        if (optPaciente.isPresent()) {
+            // Paso 3. Si existe lanzo error que ya existe el paciente
+            throw new BadRequestException("El paciente ya existe.");
+        }
+        // Paso 4. Creo el paciente y seteo los campos que lleguen del post
+        Paciente nuevo = new Paciente();
+        nuevo.setUsuarioId(pacienteNuevo.getUsuarioId());
+        nuevo.setTipoDocumento(pacienteNuevo.getTipoDocumento().toUpperCase());
+        nuevo.setNumeroDocumento(pacienteNuevo.getNumeroDocumento());
+        nuevo.setNombres(pacienteNuevo.getNombres());
+        nuevo.setApellidos(pacienteNuevo.getApellidos());
+        nuevo.setFechaNacimiento(pacienteNuevo.getFechaNacimiento());
+        nuevo.setGenero(pacienteNuevo.getGenero().toUpperCase());
+        nuevo.setTelefono(pacienteNuevo.getTelefono());
+        nuevo.setDireccion(pacienteNuevo.getDireccion());
+
+        this.pacienteRepository.save(nuevo);
+
+        // Paso 5. Devuelve respuesta ok
+        RespuestaRs rta = new RespuestaRs();
+        rta.setMensaje("El paciente se ha guardado correctamente.");
+        rta.setStatus(200);
+        return rta;
+    }
+
+
+    @Override
+    public RespuestaRs actualizarPaciente(PacienteRq pacienteNuevo) throws BadRequestException {
+        // Paso 1. Verificar que el Numero de documento del paciente venga y pertenezca a un paciente
+        Optional<Paciente> optPaciente = this.pacienteRepository.findByNumeroDocumento(pacienteNuevo.getNumeroDocumento());
+        if (optPaciente.isEmpty()) {
+            throw new BadRequestException("El Numero de documento del paciente no existe.");
         }
 
-        return optUser.get();
+        Paciente pacienteActualizar = optPaciente.get();
+        if (!pacienteActualizar.getNumeroDocumento()
+                .equals(pacienteNuevo.getNumeroDocumento())) {
+            // Cambio el numero de documento
+            Optional<Paciente> optNumeroDocumento = this.pacienteRepository
+                    .findByNumeroDocumento(pacienteNuevo.getNumeroDocumento());
+            if (optNumeroDocumento.isPresent()) {
+                throw new BadRequestException("El Numero de Documento de paciente ya existe.");
+            }
+        }
+
+        this.validarCampos(pacienteNuevo);
+
+        pacienteActualizar.setUsuarioId(pacienteNuevo.getUsuarioId());
+        pacienteActualizar.setTipoDocumento(pacienteNuevo.getTipoDocumento().toUpperCase());
+        pacienteActualizar.setNumeroDocumento(pacienteNuevo.getNumeroDocumento());
+        pacienteActualizar.setNombres(pacienteNuevo.getNombres());
+        pacienteActualizar.setApellidos(pacienteNuevo.getApellidos());
+        pacienteActualizar.setFechaNacimiento(pacienteNuevo.getFechaNacimiento());
+        pacienteActualizar.setGenero(pacienteNuevo.getGenero().toUpperCase());
+        pacienteActualizar.setTelefono(pacienteNuevo.getTelefono());
+        pacienteActualizar.setDireccion(pacienteNuevo.getDireccion());
+
+        this.pacienteRepository.save(pacienteActualizar);
+
+        RespuestaRs rta = new RespuestaRs();
+        rta.setMensaje("El paciente se ha actualizado correctamente.");
+        rta.setStatus(200);
+        return rta;
     }
+
+
+
+    private void validarCampos(PacienteRq pacienteNuevo)
+            throws BadRequestException {
+        if (pacienteNuevo.getUsuarioId() == null) {
+            throw new BadRequestException("El campo UsuarioId es obligatorio.");
+        }
+        if (pacienteNuevo.getTipoDocumento() == null
+                || pacienteNuevo.getTipoDocumento().isBlank()
+                || pacienteNuevo.getTipoDocumento().isEmpty()) {
+            throw new BadRequestException("El campo TipoDocumento es obligatorio.");
+        }
+        if (pacienteNuevo.getNumeroDocumento() == null
+                || pacienteNuevo.getNumeroDocumento().isBlank()
+                || pacienteNuevo.getNumeroDocumento().isEmpty()) {
+            throw new BadRequestException("El campo NumeroDocumento es obligatorio.");
+        }
+        if (pacienteNuevo.getNombres() == null
+                || pacienteNuevo.getNombres().isBlank()
+                || pacienteNuevo.getNombres().isEmpty()) {
+            throw new BadRequestException("El campo Nombres es obligatorio.");
+        }
+        if (pacienteNuevo.getApellidos() == null
+                || pacienteNuevo.getApellidos().isBlank()
+                || pacienteNuevo.getApellidos().isEmpty()) {
+            throw new BadRequestException("El campo Apellidos es obligatorio.");
+        }
+        if (pacienteNuevo.getFechaNacimiento() == null
+                || pacienteNuevo.getFechaNacimiento().isBlank()
+                || pacienteNuevo.getFechaNacimiento().isEmpty()) {
+            throw new BadRequestException("El campo FechaNacimiento es obligatorio.");
+        }
+        if (pacienteNuevo.getGenero() == null
+                || pacienteNuevo.getGenero().isBlank()
+                || pacienteNuevo.getGenero().isEmpty()) {
+            throw new BadRequestException("El campo Genero es obligatorio.");
+        }
+        if (pacienteNuevo.getTelefono() == null
+                || pacienteNuevo.getTelefono().isBlank()
+                || pacienteNuevo.getTelefono().isEmpty()) {
+            throw new BadRequestException("El campo Telefono es obligatorio.");
+        }
+        if (pacienteNuevo.getDireccion() == null
+                || pacienteNuevo.getDireccion().isBlank()
+                || pacienteNuevo.getDireccion().isEmpty()) {
+            throw new BadRequestException("El campo Direccion es obligatorio.");
+        }
+    }
+
 }
