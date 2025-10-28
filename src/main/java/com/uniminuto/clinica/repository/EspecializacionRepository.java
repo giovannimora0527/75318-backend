8f4fc84f9117678be220ab9c3395f9c9398de700
mauricio-xf <mauricio1234madera@gmail.com>
2025-10-28 13:34:31 -0500

    Update EspecializacionRepository.java

--- src/main/java/com/uniminuto/clinica/repository/EspecializacionRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/EspecializacionRepository.java
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
-=======
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 package com.uniminuto.clinica.repository;
 
 import com.uniminuto.clinica.entity.Especializacion;
@@ -15,19 +7,11 @@ import org.springframework.stereotype.Repository;
 
 /**
  *
-<<<<<<< HEAD
- * @author crash
- */
-@Repository
-public interface EspecializacionRepository 
-    extends JpaRepository<Especializacion, Long>{
-=======
  * @author lmora
  */
 @Repository
 public interface EspecializacionRepository 
         extends JpaRepository<Especializacion, Long> {
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
     
     Optional<Especializacion> findByCodigoEspecializacion(String codigo);
     
