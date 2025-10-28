0aac7d465d6cb29d463d352f37fea7ba7f20ced9
mauricio-xf <mauricio1234madera@gmail.com>
2025-10-28 15:05:43 -0500

    Update MedicoService.java

--- src/main/java/com/uniminuto/clinica/service/MedicoService.java
+++ src/main/java/com/uniminuto/clinica/service/MedicoService.java
@@ -1,16 +1,26 @@
 package com.uniminuto.clinica.service;
 
-import com.uniminuto.clinica.entity.Medicamento;
-import com.uniminuto.clinica.model.MedicamentoRq;
+import com.uniminuto.clinica.entity.Medico;
+import java.util.List;
+
+import com.uniminuto.clinica.model.MedicoRq;
 import com.uniminuto.clinica.model.RespuestaRs;
 import org.apache.coyote.BadRequestException;
 
-import java.util.List;
+/**
+ *
+ * @author lmora
+ */
+public interface MedicoService {
+   List<Medico> listarMedicos(); 
+   
+   List<Medico> buscarPorEspecialidad(String codigo) 
+           throws BadRequestException;
 
-public interface MedicamentoService {
-    List<Medicamento> listarMedicamentos();
+   Medico buscarMedicoPorDocumento(String documento) throws BadRequestException;
 
-    RespuestaRs guardarMedicamento(MedicamentoRq medicamentoRq) throws BadRequestException;
+   RespuestaRs guardarMedico(MedicoRq medicoNuevo) throws BadRequestException;
 
-    RespuestaRs actualizarMedicamento(MedicamentoRq medicamentoRq) throws BadRequestException;
+   RespuestaRs actualizarMedico(MedicoRq medicoNuevo) throws BadRequestException;
+
 }
