7784b8beefbf8c8406fd329abf220db8eba21f7a
mauricio-xf <mauricio1234madera@gmail.com>
2025-10-28 14:19:19 -0500

    Update PacienteRepository.java

--- src/main/java/com/uniminuto/clinica/repository/PacienteRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/PacienteRepository.java
@@ -1,33 +1,18 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-=======
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 package com.uniminuto.clinica.repository;
 
 import com.uniminuto.clinica.entity.Paciente;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
-/**
- *
- * @author crash
- */
-@Repository
-public interface PacienteRepository extends JpaRepository<Paciente, Integer>{
-    //Funcion buscar por documento identidad
-    Optional<Paciente> findByNumeroDocumento(String numeroDocumento);
+import java.util.List;
+import java.util.Optional;
 
-    Optional<Paciente> findById(Long id);
-}
-=======
 @Repository
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
 
-    Optional<Paciente> findByNumeroDocumento(String numeroDocumento);
+    Optional<Paciente> findByNumeroDocumento(String documento);
+
+    List<Paciente> findAllByOrderByFechaNacimientoAsc();
+
+    List<Paciente> findAllByOrderByFechaNacimientoDesc();
 }
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
