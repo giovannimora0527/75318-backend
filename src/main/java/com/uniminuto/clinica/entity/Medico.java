93af827a1cf7a2d5c835d575fc98653d81f200bf
mauricio-xf <mauricio1234madera@gmail.com>
2025-10-28 12:50:28 -0500

    Update Medico.java

--- src/main/java/com/uniminuto/clinica/entity/Medico.java
+++ src/main/java/com/uniminuto/clinica/entity/Medico.java
@@ -1,56 +1,28 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-=======
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 package com.uniminuto.clinica.entity;
 
-import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  *
-<<<<<<< HEAD
-<<<<<<< HEAD
- * @author crash
-=======
  * @author lmora
->>>>>>> gmora
-=======
- * @author lmora
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
  */
 @Data
 @Entity
 @Table(name = "medico")
-<<<<<<< HEAD
-public class Medico implements Serializable{
-    private static final long serialVersionUID = 1L;
-    
-=======
 public class Medico implements Serializable {
 
     /**
      * Id serializable.
      */
     private static final long serialVersionUID = 1L;
->>>>>>> 602c738be275f7f1826ccf9ef7bcb734aeea96d1
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Integer id;
     
     @Column(name = "tipo_documento")
     private String tipoDocumento;
