313d3b3ab1f3d193b966475ea79d2245823bcb5e
estevez10 <juanpabloestevez98@gmail.com>
2025-11-23 22:46:40 -0500

    Update MedicoRepository.java

--- src/main/java/com/uniminuto/clinica/repository/MedicoRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/MedicoRepository.java
@@ -1,9 +1,10 @@
-package com.uniminuto.clinica.repository;
+ppackage com.uniminuto.clinica.repository;
 
 import com.uniminuto.clinica.entity.Especializacion;
 import com.uniminuto.clinica.entity.Medico;
 import java.util.List;
 import java.util.Optional;
+
 
 import com.uniminuto.clinica.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,5 +20,8 @@ public interface MedicoRepository extends JpaRepository<Medico, Integer> {
     Optional<Medico> findByDocumento(String documento);
 
     List<Medico> findByEspecializacion(Especializacion e);
+
+
+    Optional<Medico> findByRegistroProfesional(String registroProfesional);
 
 }
