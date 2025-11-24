3d8c905952ba461bb3c9b8e346a81306b5970596
estevez10 <juanpabloestevez98@gmail.com>
2025-11-23 22:27:01 -0500

    Update MedicoApi.java

--- src/main/java/com/uniminuto/clinica/api/MedicoApi.java
+++ src/main/java/com/uniminuto/clinica/api/MedicoApi.java
@@ -1,13 +1,24 @@
 package com.uniminuto.clinica.api;
 
 import com.uniminuto.clinica.entity.Medico;
+
 import java.util.List;
 
 import com.uniminuto.clinica.model.MedicoRq;
 import com.uniminuto.clinica.model.RespuestaRs;
+import com.uniminuto.clinica.model.UsuarioRq;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  *
@@ -16,20 +27,28 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 @RequestMapping("/medico")
 public interface MedicoApi {
-    
+
     @RequestMapping(value = "/listar",
             produces = {"application/json"},
             consumes = {"application/json"},
             method = RequestMethod.GET)
     ResponseEntity<List<Medico>> listarMedicos();
-    
-    
+
+
     @RequestMapping(value = "/listar-x-cod-esp",
             produces = {"application/json"},
             consumes = {"application/json"},
             method = RequestMethod.GET)
     ResponseEntity<List<Medico>> listarMedicosporEspecialidad(
-      @RequestParam String codigo
+            @RequestParam String codigo
+    ) throws BadRequestException;
+
+    @RequestMapping(value = "/guardar",
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = RequestMethod.POST)
+    ResponseEntity<RespuestaRs> guardarMedico(
+            @RequestBody @Valid MedicoRq medicoRq
     ) throws BadRequestException;
 
     @RequestMapping(value = "/buscar-medico-documento",
@@ -39,18 +58,6 @@ public interface MedicoApi {
     ResponseEntity<Medico> buscarMedicoPorDocumento(
             @RequestParam String documento)
             throws BadRequestException;
-
-
-    /*
-     * TODO Agregar medodo para guardar medico.*/
-    @RequestMapping(value = "/guardar",
-        produces = {"application/json"},
-        consumes = {"application/json"},
-        method = RequestMethod.POST)
-    ResponseEntity<RespuestaRs> guardarMedico(
-       @RequestBody MedicoRq medicoNuevo
-    ) throws BadRequestException;
-
 
 
 
@@ -63,5 +70,4 @@ public interface MedicoApi {
     ResponseEntity<RespuestaRs> actualizarMedico(
             @RequestBody MedicoRq medico
     ) throws BadRequestException;
-
 }
